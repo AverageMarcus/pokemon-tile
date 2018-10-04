41e8b986bd12db5f17713bbe082d871c45cb207f
Marcus Noble <github@marcusnoble.co.uk>
2018-10-04 06:31:00 +0100

    Added serviceworker

---
+++ serviceworker.js
@@ -0,0 +1,18 @@
+var CACHE = 'v1';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || caches.open(CACHE).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
